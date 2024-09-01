bf881f25c97c845b7535efb19fd9086cb069e128
edanilenko34 <37924622+edanilenko34@users.noreply.github.com>
2024-09-01 20:31:27 +0300

    Delete src/IrigationServer directory

--- src/IrigationServer/IrigationServer.AppHost/Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-var apiService = builder.AddProject<Projects.IrigationServer_ApiService>("apiservice");
-
-builder.AddProject<Projects.IrigationServer_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithReference(apiService);
-
-builder.Build().Run();
